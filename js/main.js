0cdc7413df1df179148b29c3bddeafccc0f6ba41
Daria Liferova <liferova.daria@gmail.com>
2021-06-07 18:27:28 +0200

    Добавила функцию создания массива из сгенерированных объектов.

--- js/main.js
+++ js/main.js
@@ -35,3 +35,55 @@ const checkStringLength = function(string, maxLength) {
 };
 
 checkStringLength('Это мое домашнее задание', 50);
+
+
+const generate = function() {
+  const objects = [];
+
+  const createComment = function(id) {
+    const possibleMessages = [
+      'Всё отлично!',
+      'В целом всё неплохо. Но не всё.',
+      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+    ];
+
+    const possibleNames = [
+      'Иван',
+      'Виктор',
+      'Николай',
+      'Илья',
+      'Яна',
+      'Дарья'
+    ]
+
+    const authorId = getRandomFromRange(1, 6);
+
+    return {
+      id: id,
+      avatar: 'img/avatar-' + authorId + '.svg',
+      message: possibleMessages[getRandomFromRange(0, possibleMessages.length - 1)],
+      name: possibleNames[authorId - 1]
+    };
+  }
+
+  for (let i = 0; i < 25; i++) {
+    const id = i + 1;
+
+    objects[i] = {
+      id: id,
+      url: 'photos/' + id + '.jpg',
+      description: 'Моя новая фотография.',
+      likes: getRandomFromRange(15, 200),
+      comments: [
+        createComment(135)
+      ]
+    }
+  }
+
+  return objects;
+};
+
+console.log(generate());
