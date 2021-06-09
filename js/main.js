e812438f2097fcbaeabf47d18a30c9e509323cef
Daria Liferova <liferova.daria@gmail.com>
2021-06-09 23:18:47 +0200

    Правки функции для создания массива из сгенерированных объектов.

--- js/main.js
+++ js/main.js
@@ -36,61 +36,54 @@ const checkStringLength = function(string, maxLength) {
 
 checkStringLength('Это мое домашнее задание', 50);
 
-const generate = function() {
-  const objects = [];
+const TOTAL_PHOTOS_NUMBER = 25;
+const LIKES_MIN_NUMBER = 15;
+const LIKES_MAX_NUMBER = 200;
+const MIN_AVATAR_ID = 1;
+const MAX_AVATAR_ID = 6;
+const MIN_COMMENTS_NUMBER = 2;
+const MAX_COMMENTS_NUMBER = 7;
+const POSSIBLE_COMMENT_MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const POSSIBLE_COMMENT_NAMES = [
+  'Иван',
+  'Виктор',
+  'Николай',
+  'Илья',
+  'Яна',
+  'Дарья',
+];
+const POSSIBLE_PHOTO_DESCRIPTION = 'Это новое фото.';
 
-  const createComment = function(id) {
-    const possibleMessages = [
-      'Всё отлично!',
-      'В целом всё неплохо. Но не всё.',
-      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-    ];
+const getRandomArrayElement = (elements) => elements[getRandomFromRange(0, elements.length - 1)];
 
-    const possibleNames = [
-      'Иван',
-      'Виктор',
-      'Николай',
-      'Илья',
-      'Яна',
-      'Дарья',
-    ];
+const createComment = (id) => ({
+  id: id,
+  avatar: `img/avatar-${getRandomFromRange(MIN_AVATAR_ID, MAX_AVATAR_ID)}.svg`,
+  message: getRandomArrayElement(POSSIBLE_COMMENT_MESSAGES),
+  name: getRandomArrayElement(POSSIBLE_COMMENT_NAMES),
+});
 
-    const authorId = getRandomFromRange(1, 6);
+const generateComments = () => new Array(getRandomFromRange(MIN_COMMENTS_NUMBER, MAX_COMMENTS_NUMBER))
+  .fill(null)
+  .map((element, index) => createComment(index + 1));
 
-    return {
-      id: id,
-      avatar: `img/avatar-${  authorId  }.svg`,
-      message: possibleMessages[getRandomFromRange(0, possibleMessages.length - 1)],
-      name: possibleNames[authorId - 1],
-    };
-  };
+const createPhoto = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  description: POSSIBLE_PHOTO_DESCRIPTION,
+  likes: getRandomFromRange(LIKES_MIN_NUMBER, LIKES_MAX_NUMBER),
+  comments: generateComments(),
+});
 
-  const generateComments = function () {
-    const comments = [];
+const generatePhotos = () => new Array(TOTAL_PHOTOS_NUMBER)
+  .fill(null)
+  .map((element, index) => createPhoto(index + 1));
 
-    for (let index = 0; index < getRandomFromRange(3, 7); index++) {
-      comments.push( createComment(index + 1) );
-    }
-
-    return comments;
-  };
-
-  for (let index = 0; index < 25; index++) {
-    const id = index + 1;
-
-    objects[index] = {
-      id: id,
-      url: `photos/${  id  }.jpg`,
-      description: 'Моя новая фотография.',
-      likes: getRandomFromRange(15, 200),
-      comments: generateComments(),
-    };
-  }
-
-  return objects;
-};
-
-generate();
+generatePhotos();
