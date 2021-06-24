18bd8b4dc11ffcedb77a546488ead20a6bf1e855
Daria Liferova <liferova.daria@gmail.com>
2021-06-25 00:45:01 +0200

    Добавила в проект валидацию проверки введённых данных.

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,15 @@
 import {generatePhotos} from './data.js';
 import {renderThumbnails} from './photo-thumbnails.js';
-import {openModal, photoNumber} from './photo-detail-modal.js';
+import {openForm, validateCommentInForm, validateHashtags} from './form.js';
 
 const photos = generatePhotos();
 renderThumbnails(photos);
-openModal(photos[photoNumber]);
+// openModal(photos[photoNumber]);
+
+const openFormElement = document.querySelector('.img-upload__input');
+openFormElement.addEventListener('change', () => {
+  openForm();
+});
+
+validateHashtags();
+validateCommentInForm();
