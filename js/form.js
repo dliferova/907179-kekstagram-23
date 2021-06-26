fd3e30c552a9334233d0d3cb38fcb36812170978
Daria Liferova <liferova.daria@gmail.com>
2021-06-26 18:05:07 +0200

    Доработки задания.

--- js/form.js
+++ js/form.js
@@ -3,9 +3,10 @@ import {isEscEvent, checkStringLength} from './util.js';
 const REG_HASTAG = /^#[A-Za-zА-Яа-я0-9]{1,19}$/;
 const MAX_HASTAGS_COUNT = 5;
 const MAX_COMMENT_LENGTH = 140;
+
 const uploadForm = document.querySelector('.img-upload__form');
 const uploadOverlay = uploadForm.querySelector('.img-upload__overlay');
-const uploadCancel = uploadForm.querySelector('#upload-cancel');
+const uploadCancel = uploadForm.querySelector('.img-upload__cancel');
 const uploadText = uploadForm.querySelector('.img-upload__text');
 const textHashtags = uploadText.querySelector('.text__hashtags');
 const commentField = uploadText.querySelector('.text__description');
@@ -21,9 +22,7 @@ const closeForm = () => {
   document.body.classList.remove('modal-open');
 };
 
-uploadCancel.addEventListener('click', () => {
-  closeForm();
-});
+uploadCancel.addEventListener('click', closeForm);
 
 const onKeyPress = (evt) => {
   if (isEscEvent(evt) && textHashtags !== document.activeElement) {
@@ -34,43 +33,32 @@ const onKeyPress = (evt) => {
 
 document.addEventListener('keydown', onKeyPress);
 
-export const validateHashtags = () => {
-  textHashtags.addEventListener('input', () => {
+textHashtags.addEventListener('input', () => {
 
-    const value = textHashtags.value.toLowerCase();
-    const hashtags = value.split(' ');
+  const value = textHashtags.value.toLowerCase();
+  const hashtags = value.split(' ');
+  const hasInvalidHashtag = hashtags.some((hashtag) => !REG_HASTAG.test(hashtag));
 
-    const allHashtagsValid = function() {
-      for (let el = 0; el < hashtags.length; el++) {
-        const test = REG_HASTAG.test(hashtags[el]);
-        if (!test) {
-          return false;
-        }
-      }
-      return true;
-    };
+  if (hasInvalidHashtag) {
+    textHashtags.setCustomValidity('Используйте после # буквы и числа без пробелов, спецсимволы (#, @, $ и т. п.) и символы пунктуации');
+  } else if (hashtags.length !== new Set(hashtags).size) {
+    textHashtags.setCustomValidity('Хештег уже используется');
+  } else if (hashtags.length > MAX_HASTAGS_COUNT) {
+    textHashtags.setCustomValidity('Вы не можете использовать более 5 хештегов');
+  } else {
+    textHashtags.setCustomValidity('');
+  }
 
-    if (!allHashtagsValid()) {
-      textHashtags.setCustomValidity('Используйте после # буквы и числа без пробелов, спецсимволы (#, @, $ и т. п.) и символы пунктуации');
-    } else if (hashtags.length !== new Set(hashtags).size) {
-      textHashtags.setCustomValidity('Хештег уже используется');
-    } else if (hashtags.length > MAX_HASTAGS_COUNT) {
-      textHashtags.setCustomValidity('Вы не можете использовать более 5 хештегов');
-    } else {
-      textHashtags.setCustomValidity('');
-    }
-    textHashtags.reportValidity();
-  });
-};
+  textHashtags.reportValidity();
+});
 
-export const validateCommentInForm = () => {
-  commentField.addEventListener('input', () => {
+commentField.addEventListener('input', () => {
 
-    if(!checkStringLength(commentField.value, MAX_COMMENT_LENGTH)) {
-      commentField.setCustomValidity(`Длина вашего комментария не может быть больше ${ MAX_COMMENT_LENGTH } символов`);
-    } else {
-      commentField.setCustomValidity('');
-    }
-    commentField.reportValidity();
-  });
-};
+  if (!checkStringLength(commentField.value, MAX_COMMENT_LENGTH)) {
+    commentField.setCustomValidity(`Длина вашего комментария не может быть больше ${MAX_COMMENT_LENGTH} символов`);
+  } else {
+    commentField.setCustomValidity('');
+  }
+
+  commentField.reportValidity();
+});
--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,9 @@
 import {generatePhotos} from './data.js';
 import {renderThumbnails} from './photo-thumbnails.js';
-import {openForm, validateCommentInForm, validateHashtags} from './form.js';
+import {openForm} from './form.js';
 
 const photos = generatePhotos();
 renderThumbnails(photos);
-// openModal(photos[photoNumber]);
 
 const openFormElement = document.querySelector('.img-upload__input');
-openFormElement.addEventListener('change', () => {
-  openForm();
-});
-
-validateHashtags();
-validateCommentInForm();
+openFormElement.addEventListener('change', openForm);
